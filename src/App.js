b34512b1eb687267a99823255dbc208bf712734a
brittrestine <brittrestine@gmail.com>
2020-09-02 09:43:26 -0500

    wip with form design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import './App.css';
 
-import React from 'react';
+import React, {useState} from 'react';
+
+import Cards from 'react-credit-cards';
 
 function App() {
+const [cvc, setCvc] = useState('');
+const [expiry, setExpiry] = useState('');
+const [focus, setFocus] = useState('');
+const [name, setName] = useState('');
+const [number, setNumber] = useState('');
+console.log("name", name)
+console.log("number", number)
+console.log("focus", focus)
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"/>
 
-      </header>
+      <div className="PaymentForm">
+        <Cards
+          className="Card"
+          cvc={cvc}
+          expiry={expiry}
+          focused={focus}
+          name={name}
+          number={number}
+        />
+
+        <form className="Form">
+        	<input
+            className="Input"
+            type="tel"
+            name="number"
+            placeholder="Card Number"
+            onChange={(e) => setNumber(e.target.value)}
+            onFocus={(e) => setFocus({ focus: e.target.name })}
+          />
+
+        	<input
+            className="Input"
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={(e) => setName( e.target.value)}
+          />
+
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
